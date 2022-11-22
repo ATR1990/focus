2710bc84fb2e0bab3311cb808ade222550f434fb
AltynbekovT <tauyekelaltynbekov@gmail.com>
2022-11-22 18:14:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react"
 
-function App() {
+export const App = () => {
+  const firstInputRef = useRef()
+
+  useEffect(() => {
+    console.log(firstInputRef)
+    console.log(firstInputRef.current)
+  })
+
+  const setFocusOnFirstInput = event => {
+    if (event.key === 'Tab') {
+      event.preventDefault()
+      firstInputRef.current.focus()
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      <input type="text" ref={firstInputRef} />
+      <input type="text" />
+      <input type="text" onKeyDown={setFocusOnFirstInput} />
+    </form>
+  )
 }
-
-export default App;
