e593d5c2ae9b3fdc21fc4738501cb8f5202ab87f
AltynbekovT <tauyekelaltynbekov@gmail.com>
2022-11-22 18:42:31 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react"
 
 export const App = () => {
   const firstInputRef = useRef()
+  const secondInputRef = useRef()
 
   useEffect(() => {
     console.log(firstInputRef)
@@ -13,13 +14,18 @@ export const App = () => {
       event.preventDefault()
       firstInputRef.current.focus()
     }
-  };
+  }
+
+  const changeColorBorder = () => {
+    secondInputRef.current.style.border = '2px solid red'
+  }
 
   return (
     <form>
-      <input type="text" ref={firstInputRef} />
-      <input type="text" />
+      <input type="text" ref={firstInputRef} autoFocus />
+      <input type="text" ref={secondInputRef} />
       <input type="text" onKeyDown={setFocusOnFirstInput} />
+      <button type="button" onClick={changeColorBorder}>Change color border for second input</button>
     </form>
   )
 }
